32b30277a85296dd77bc9a55fac8643d21557c82
greg <thorn@myactv.net>
2018-02-02 19:55:36 -0500

    Some imports where missing

--- thymeleaf/src/main/java/org/allstar/spring/config/SecurityConfig.java
+++ thymeleaf/src/main/java/org/allstar/spring/config/SecurityConfig.java
@@ -2,14 +2,16 @@ package org.allstar.spring.config;
 
 import org.allstar.spring.web.LoggingAccessDeniedHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /* security */
-@Configureation
+@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
